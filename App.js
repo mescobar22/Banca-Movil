bc44eb7e54dee3598229077dc783dfc7582137b9
Moises <mescobar_22@alu.uabcs.mx>
2024-11-19 11:26:01 -0700

    Design creation for register.js

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { FlatListComponent, View } from 'react-native';
+import { View } from 'react-native';
 import Login from './Screens/login';
-
-const Stack = createNativeStackNavigator();
+import Register from './Screens/register';
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='login'
-        screenOptions={{
-          headerShown: FlatListComponent,
-        }}
-      ></Stack.Navigator> 
-        <Stack.Screen name='login' component={login}/>
-        <Stack.Screen name='register' component={register}/>
-    </NavigationContainer>
+    <View style={ {flex: 1}}>
+      <Register/>
+      <StatusBar style="auto" />
+    </View>
   );
 };
---
+++ Screens/register.js
@@ -0,0 +1,96 @@
+import { Text, TouchableOpacity, TextInput, View, StyleSheet, StatusBar} from "react-native";
+
+export default function Register() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.txt_1}>Banca</Text>
+      <Text style={styles.txt_2}>móvil</Text>
+      <View style={styles.border}>
+        <Text style={styles.txt_3}>Register</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=" Name"
+          placeholderTextColor="#abb3c1"
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          placeholder=" Last name"
+          placeholderTextColor="#abb3c1"
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          placeholder=" Email"
+          placeholderTextColor="#abb3c1"
+        ></TextInput>
+        <TextInput
+          style={styles.input}
+          placeholder=" Password"
+          placeholderTextColor="#abb3c1"
+        ></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.text_button}>Sign Up</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 80,
+    flex: 1,
+    backgroundColor: "#001b48",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  border: {
+    flex: 1,
+    alignItems: "center",
+    borderColor: "#ff3030",
+    borderTopRightRadius: 60,
+    borderTopLeftRadius: 60,
+    backgroundColor: "#ffffff",
+    width: "170%",
+    marginBottom: "-50%",
+    padding: 20,
+  },
+  txt_1: {
+    marginBottom: -32,
+    fontSize: 70,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  txt_2: {
+    marginStart: -45,
+    fontSize: 60,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  txt_3: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "#011c49",
+    paddingBottom: 35,
+  },
+  input: {
+    fontSize: 20,
+    fontWeight: "bold",
+    backgroundColor: "#f9f8f8",
+    width: "100%",
+    margin: 12,
+  },
+  button: {
+    marginTop: "20%",
+    borderWidth: 2,
+    backgroundColor: "#001b48",
+    alignItems: "center",
+    borderRadius: 25,
+    width: "90%",
+    padding: 15,
+  },
+  text_button: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+});
