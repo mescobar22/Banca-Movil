761211ac6cdc4f3c8c041173c253498cbce47af6
Moises <mescobar_22@alu.uabcs.mx>
2024-11-22 12:55:38 -0700

    Update Design Transfer.js

--- Screens/4_transfer.js
+++ Screens/4_transfer.js
@@ -1,4 +1,4 @@
-// Comprobación de la estructura de css ver si es correcta y la funcionalidad de el boton, poner en numerico el teclado y letra de acuerdo al input
+// Iconos / Se mueve las letras al iniciar el teclado del movil
 
 import {
   Text,
@@ -9,7 +9,7 @@ import {
   StatusBar,
 } from "react-native";
 
-export default function Register() {
+export default function Transfer() {
   return (
     <View style={styles.container}>
       
@@ -26,6 +26,7 @@ export default function Register() {
             style={styles.input}
             placeholder="              $0 MXN"
             placeholderTextColor="#ffffff"
+            keyboardType="numeric"
         ></TextInput>
         
         <Text style={styles.txt_3}>Reference</Text>
@@ -34,6 +35,7 @@ export default function Register() {
             style={styles.input_2}
             placeholder="#000000"
             placeholderTextColor="#ffffff"
+            keyboardType="numeric"
         ></TextInput>
 
         <Text style={styles.txt_4}>Concept</Text>
@@ -41,9 +43,13 @@ export default function Register() {
         <TextInput
             style={styles.input_2}
             placeholderTextColor="#ffffff"
+            keyboardType="default"
         ></TextInput>
-        
-        <Text style={styles.txt_5}>You can write up to 40 characters</Text>
+
+        <View style={styles.row_container}>
+          <Text style={styles.txt_5}>You can write up to 40 characters</Text>
+          <Text style={styles.txt_6}>0/40</Text>
+        </View>
 
         <TouchableOpacity style={styles.button}>
             <Text style={styles.text_button}>Continue</Text>
@@ -91,28 +97,18 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   txt_3: {
-    fontSize: 17,
-    fontWeight: "bold",
+    fontSize: 16,
     color: "#ffffff",
     marginTop: 40,
     marginBottom: -10,
-    marginStart: -235,
+    marginStart: -255,
   },
   txt_4: {
-    fontSize: 17,
-    fontWeight: "bold",
+    fontSize: 16,
     color: "#ffffff",
     marginTop: 40,
     marginBottom: -10,
-    marginStart: -250,
-  },
-  txt_5: {
-    fontSize: 9,
-    fontWeight: "bold",
-    color: "#ffffff",
-    marginTop: 40,
-    marginBottom: -10,
-    marginStart: -250,
+    marginStart: -260,
   },
   input: {
     fontSize: 30,
@@ -124,22 +120,40 @@ const styles = StyleSheet.create({
     color: "#ffffff",
   },
   input_2: {
+    paddingTop: 25,
     fontSize: 20,
-    fontWeight: "bold",
     width: "140%",
     height: 60,
     borderBottomWidth: 1,
     borderColor: "#ffffff",
     color: "#ffffff",
   },
+  row_container: {
+    position: "absolute",
+    bottom: 220,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  txt_5: {
+    fontSize: 9,
+    color: "#ffffff",
+    marginTop: 10,
+    marginStart: -160,
+  },
+  txt_6: {
+    fontSize: 9,
+    color: "#ffffff",
+    marginTop: 10,
+    marginEnd: -160,
+  },
   button: {
-    marginTop: 25,
+    marginTop: 85,
     backgroundColor: "#ffffff",
     alignItems: "center",
     borderRadius: 25,
     width: "120%",
     padding: 15,
-    marginBottom: 45,
+    marginBottom: 55,
   },
   text_button: {
     fontSize: 20,
